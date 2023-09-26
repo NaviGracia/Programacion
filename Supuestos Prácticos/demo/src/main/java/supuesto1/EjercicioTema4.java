4c6530da5cfdac8b4653c545f5828288e6f00c93
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-26 13:02:18 +0200

    supuesto practico 1

---
+++ Supuestos Prácticos/demo/src/main/java/supuesto1/EjercicioTema4.java
@@ -0,0 +1,16 @@
+package supuesto1;
+
+public class EjercicioTema4 {
+    public static void main(String[] args) {
+        final int CONS1 = 1008;
+        final String CONS2 = " Saludos";
+        Integer variable1 = CONS1;
+        Integer variable2 = variable1;
+        System.out.println("Y las variables son " + variable2 + CONS2);
+    }
+
+    /*Actividad Ampliación 1 */
+    final int CONS3 = 1;
+    final String CONS4 = "Hola";
+    System.out.
+}
