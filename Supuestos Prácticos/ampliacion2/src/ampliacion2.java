2e496ba02f98d7e640dff354406cc1565c74f42f
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 13:34:23 +0200

    j

---
+++ Supuestos Prácticos/ampliacion2/src/ampliacion2.java
@@ -0,0 +1,9 @@
+public class ampliacion2 {
+    public static void main(String[] args) throws Exception {
+        int b = 9<<2;
+        System.out.println(b);
+        /* 9<<2 hace que el 9 se mueva dos posiciones a la izquierda en bits,
+         es decir, 900 bits, que en decimal seria 9*4 = 36 
+         */
+    }
+}
