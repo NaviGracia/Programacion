79a4bd449f46cedf185899de798c8382fe7d0fa2
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 14:04:06 +0200

    x

---
+++ Supuestos Prácticos/ampliacion5/src/ampliacion5.java
@@ -0,0 +1,14 @@
+public class ampliacion5 {
+    public static void main(String[] args) throws Exception {
+        int $hora;
+        int 1LoveYou;
+        /*No se acepta que el primer dígito sea un número */
+        int Calcula_fecha;
+        int calcula Fecha;
+        /*No se aceptan espacios */
+        int calculaFecha;
+        int IVA;
+        int iva%;
+        /*No se acepta % */
+    }
+}
