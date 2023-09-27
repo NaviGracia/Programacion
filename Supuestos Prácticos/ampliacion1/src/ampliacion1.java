0be6aee0606d2b7ab4c0a78e4a4fc1dd9f04090d
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 13:28:46 +0200

    j

--- Supuestos Prácticos/ampliacion1/src/ampliacion1.java
+++ Supuestos Prácticos/ampliacion1/src/ampliacion1.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class ampliacion1 {
+    public static void main(String[] args) {
+       /*Actividad Ampliación 2 */
+       int b = 9<<2;
+       System.out.println(b);
+       /* 9<<2 hace que el 9 se mueva dos posiciones a la izquierda en bits,
+        es decir, 900 bits, que en decimal seria 9*4 = 36 
+        */
     }
 }
---
+++ Supuestos Prácticos/demo/src/main/java/supuesto1/EjercicioTema4.java
@@ -0,0 +1,20 @@
+package supuesto1;
+
+public class EjercicioTema4 {
+
+    public static void main(String[] args) {
+        final int CONS1 = 1008;
+        final String CONS2 = " Saludos";
+        Integer variable1 = CONS1;
+        Integer variable2 = variable1;
+        System.out.println("Y las variables son " + variable2 + CONS2);
+        
+        /*Actividad Ampliación 3 */
+        int b2 = 16&8;
+        System.out.println(b2);
+        /* 9<<2 hace que el 9 se mueva dos posiciones a la izquierda en bits,
+         es decir, 900 bits, que en decimal seria 9*4 = 36 
+         */
+    }
+}
+    
