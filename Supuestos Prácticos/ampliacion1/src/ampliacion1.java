b3451d7adc995de5cfb8c54949208f853c3e9df3
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 13:31:47 +0200

    j

--- Supuestos Prácticos/ampliacion1/src/ampliacion1.java
+++ Supuestos Prácticos/ampliacion1/src/ampliacion1.java
@@ -1,10 +1,7 @@
 public class ampliacion1 {
-    public static void main(String[] args) {
-       /*Actividad Ampliación 2 */
-       int b = 9<<2;
-       System.out.println(b);
-       /* 9<<2 hace que el 9 se mueva dos posiciones a la izquierda en bits,
-        es decir, 900 bits, que en decimal seria 9*4 = 36 
-        */
+    static final int CONS1 = 1;
+    static final String CONS2 = "Hola ";
+    public static void main(String[] args) throws Exception {
+        System.out.println("Las constantes son" + CONS2 + CONS1);
     }
 }
