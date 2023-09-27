dd457f54f35f3dcaea1aa81d6921003407618ce8
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 13:51:09 +0200

    f

---
+++ Supuestos Prácticos/ampliacion4/src/ampliacion4.java
@@ -0,0 +1,8 @@
+public class ampliacion4 {
+    public static void main(String[] args) throws Exception {
+        int b = 15;
+        int a = b++ + 13;
+        System.out.println(a);
+        /* El valor de b + 13, y si se repitiera el proceso se iria incrementando +1 */
+    }
+}
