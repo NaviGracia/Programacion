1b7cba11a5a1ae2b95be24f3464fd1a6483d99f3
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 13:43:35 +0200

    h

--- Supuestos Prácticos/ampliacion3/src/ampliacion3.java
+++ Supuestos Prácticos/ampliacion3/src/ampliacion3.java
@@ -1,5 +1,7 @@
 public class ampliacion3 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int b=16&8;
+        System.out.println(b);
+        /*Te devuelve 0 que es igual a FALSE, ya que 16 no es igual a 8 */
     }
 }
