cdb461f24efe454733680f9a887913b7d69810f2
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 14:35:15 +0200

    h

---
+++ Supuestos Prácticos/ejercicioresuelto2/src/EjercicioResuelto2.java
@@ -0,0 +1,16 @@
+public class EjercicioResuelto2 {
+    /*2.- */
+    public void main(String[] args) {
+        System.out.println("Hello, World!");
+    
+    /*Falso. No pueden haber dos mains en un mismo fichero */
+    /*3.- Verdadero */
+    /*4.- */
+        byte a =200;
+    /*Falso. Saldrá error */
+    }
+}
+/*1.- */
+
+
+/*Falso, cada clase tiene su propio fichero */
