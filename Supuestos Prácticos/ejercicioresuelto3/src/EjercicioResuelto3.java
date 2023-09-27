b26710ac908f9ba37df4515146fddd2d3216ff4d
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 14:39:51 +0200

    f

---
+++ Supuestos Prácticos/ejercicioresuelto3/src/EjercicioResuelto3.java
@@ -0,0 +1,10 @@
+public class EjercicioResuelto3 {
+    public static void main(String[] args) throws Exception {
+        int a = 10;
+        int b = 1;
+        int tmp = a;
+        a=b;
+        b=tmp;
+        System.out.println("a es " + a + " y b es " + b);;
+    }
+}
