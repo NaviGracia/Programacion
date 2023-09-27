57e245c25024c1af18722f983f529db3a87974b0
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-27 14:46:41 +0200

    f

---
+++ Supuestos Prácticos/ejercicioresuelto4/src/joven.java
@@ -0,0 +1,11 @@
+public class joven {
+    int edad;
+    int nivel_de_estudios;
+    int ingresos;
+
+    public static void main(String[] args) throws Exception {
+        boolean jasp = false;
+        jasp = ((edad<=28)&&(nivel_de_estudios>3)&&())
+        System.out.println("Hello, World!");
+    }
+}
