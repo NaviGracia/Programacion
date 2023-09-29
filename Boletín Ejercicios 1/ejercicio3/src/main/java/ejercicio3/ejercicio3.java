36f3ed8b0a6555b6d760b1f645e8a8d00d097213
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-29 11:42:18 +0200

    a

---
+++ Boletín Ejercicios 1/ejercicio3/src/main/java/ejercicio3/ejercicio3.java
@@ -0,0 +1,9 @@
+package ejercicio3;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        int n = 1;
+        int n2 = (n + 77 - 3)*2;
+        System.out.println("La operación de (n + 77 - 3)*2 = " + n2);
+    }
+}
