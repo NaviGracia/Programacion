7d6a451950b7bff862827459878caa75e6ae85cb
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-29 12:54:05 +0200

    g

---
+++ Boletín Ejercicios 1/ejercicio5/src/main/java/ejercicio5/ejercicio5.java
@@ -0,0 +1,11 @@
+package ejercicio5;
+
+public class ejercicio5 {
+    public static void main(String[] args) {
+        int a = 4;
+        int a2 = a%2;
+        String estado = a2 <= 0 ? "Par" : "Impar";
+        System.out.println(estado);
+        }
+    
+}
