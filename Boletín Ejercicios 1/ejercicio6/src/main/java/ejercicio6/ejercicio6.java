784cdd2ddabc2101cd7ce0583f897b9c25228605
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-29 13:03:19 +0200

    a

--- Boletín Ejercicios 1/ejercicio6/src/main/java/ejercicio6/ejercicio6.java
+++ Boletín Ejercicios 1/ejercicio6/src/main/java/ejercicio6/ejercicio6.java
@@ -2,7 +2,7 @@ package ejercicio6;
 
 public class ejercicio6 {
     public static void main(String[] args) {
-        int b = 6;
-        System.out.println("Hello world!");
+        int b = -6;
+        System.out.println("El valor " + b + " es " + (b >= 0 ? "Positivo" : "Negativo"));
     }
 }
