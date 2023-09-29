08d4632ed767a59f7b56fedc8a72597acce95984
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-29 10:29:01 +0200

    l

---
+++ Boletín Ejercicios 1/ejercicio1/src/ejercicio1.java
@@ -0,0 +1,12 @@
+public class ejercicio1 {
+    public static void main(String[] args) throws Exception {
+        int n = 7;
+        double a = 3.4;
+        char c = 'a';
+        int d = c;
+        System.out.println("Valor n = " + n + " Valor a = " + a + " Valor c = " + c);
+        System.out.println("Suma de n + a = " + (n+a));
+        System.out.println("Resto de a - n = " + (a - n));
+        System.out.println("Valor númerico de c = " + d);
+    }
+}
