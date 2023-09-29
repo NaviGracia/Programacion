77204614251f2bc8b2b39f09e5c77f2078d2dcda
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-29 11:55:42 +0200

    a

---
+++ Boletín Ejercicios 1/ejercicio4/src/main/java/ejercicio4/ejercicio4.java
@@ -0,0 +1,16 @@
+package ejercicio4;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+        int a2 = 2;
+        int b2 = 5; 
+        int c2 = 9;
+        int d2 = 8;
+        int a = d2; 
+        int b = c2;
+        int c = a2;
+        int d = b2;
+        System.out.println("Valor de a = " + a2 + " b = " + b2 + " c = " + c2 + " d = " + d2);
+        System.out.println("Valores Intercambiados: a = " + a + " b = " + b + " c = " + c + " d = " + d);
+    }
+}
