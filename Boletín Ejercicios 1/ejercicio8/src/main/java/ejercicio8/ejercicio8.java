8cabd0211687ee7ec392af7d1a7400cd12b556c5
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-30 22:47:25 +0200

    a

--- Boletín Ejercicios 1/ejercicio8/src/main/java/ejercicio8/ejercicio8.java
+++ Boletín Ejercicios 1/ejercicio8/src/main/java/ejercicio8/ejercicio8.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 
 public class ejercicio8 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Introduzca los valores a y b");
-        Scanner a = new Scanner (System.in);
-        int b = System.in.read();
-        System.out.println("Los números son" + a + " y " + b);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("El valor a = " + a + " y b = " + b);
     }
 }
