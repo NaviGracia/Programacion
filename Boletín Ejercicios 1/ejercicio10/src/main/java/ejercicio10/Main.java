abc62af5ae26b2d2f9fec6a180a8b557e7f83a2a
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-30 23:44:31 +0200

    a

--- Boletín Ejercicios 1/ejercicio10/src/main/java/ejercicio10/Main.java
+++ Boletín Ejercicios 1/ejercicio10/src/main/java/ejercicio10/Main.java
@@ -1,7 +1,14 @@
 package ejercicio10;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Introduza un número");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int doble = (num*2);
+        int triple = (num*3);
+        System.out.println("El doble de " + num + " es " + doble + " y el triple es " + triple);
     }
 }
