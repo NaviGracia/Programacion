e63bb7870cbbf0c74779ba9ed77d638dcdd68f3a
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-09-30 18:27:31 +0200

    a

--- Boletín Ejercicios 1/ejercicio7/src/main/java/ejercicio7/ejercicio7.java
+++ Boletín Ejercicios 1/ejercicio7/src/main/java/ejercicio7/ejercicio7.java
@@ -2,10 +2,15 @@ package ejercicio7;
 
 public class ejercicio7 {
     public static void main(String[] args) {
-        int c = -5;
-        int c2 = c%2;
+        int c = 150;
+        int par = c%2;
+        int multiplo5 = c%5;
+        int multiplo10 = c%10;
         System.out.println("El valor c es " + c);
         System.out.println("Es " + (c >= 0 ? "Positivo" : "Negativo"));
-        System.out.println("Es " + (c2 == 0 ? "Par" : "Impar"));
+        System.out.println("Es " + (par == 0 ? "Par" : "Impar"));
+        System.out.println((multiplo5 == 0 ? "Es Múltiplo de 5" : "No es múltiplo de 5"));
+        System.out.println((multiplo10 == 0 ? "Es Múltiplo de 10" : "No es Múltiplo de 10"));
+        System.out.println("Es " + (c > 100 ? "Mayor que 100" : "Menor de 100"));
     }
 }
