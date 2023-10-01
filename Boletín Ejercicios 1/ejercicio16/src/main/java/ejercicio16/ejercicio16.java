7dbed19a5d16e0d8fd30b614c734f295f59ceba7
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-01 20:56:05 +0200

    a

---
+++ Boletín Ejercicios 1/ejercicio16/src/main/java/ejercicio16/ejercicio16.java
@@ -0,0 +1,8 @@
+package ejercicio16;
+
+public class ejercicio16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("");
+    }
+}
