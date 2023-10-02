f81af4720f82b41a8dfa98664b33742f06a29373
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-02 11:51:38 +0200

    d

--- Boletín Ejercicios 1/ejercicio16/src/main/java/ejercicio16/ejercicio16.java
+++ Boletín Ejercicios 1/ejercicio16/src/main/java/ejercicio16/ejercicio16.java
@@ -6,7 +6,7 @@ public class ejercicio16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        System.out.println("Introduzca un número");
+        System.out.println("Introduzca un número de tres cifras");
         int n = sc.nextInt();
         System.out.println("Primera cifra de " + n + " es " + (n/100));
         System.out.println("Cifra central de " + n + " es " + (n/10)%10);
