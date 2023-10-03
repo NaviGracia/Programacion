cda3d1b34c15d07eb2ea23506c285c72d7878cb3
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-03 12:50:43 +0200

    a

---
+++ Ejercicios Propuestos Tema 5/eje1/src/main/java/eje1/actprop.java
@@ -0,0 +1,8 @@
+package eje1;
+
+public class actprop {
+    public static void main(String[] args) {
+        System.out.println("Argumentos: " + args[0]);
+    }
+    /*Si que se puede poniendo como argumento "Hola\ Mundo" ya que la barra inversa significa un espacio*/
+}
---
+++ Ejercicios Propuestos Tema 5/eje2/src/main/java/eje2/eje2.java
@@ -0,0 +1,11 @@
+package eje2;
+
+public class eje2 {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        if ((num%4 == 0) && (num%100 != 0) || (num%400 == 0))
+            System.out.println("Es bisiesto");
+        else
+            System.out.println("No es bisiesto");
+    }
+}
