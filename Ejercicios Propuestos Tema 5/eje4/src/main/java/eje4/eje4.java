4b405e9fe7e9c2bc7f9b998aed0a2a6842f1d2fc
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-03 13:04:01 +0200

    a

---
+++ Ejercicios Propuestos Tema 5/eje4/src/main/java/eje4/eje4.java
@@ -0,0 +1,21 @@
+package eje4;
+
+public class eje4 {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        switch (num){
+            case 1: System.out.println("Enero");
+            case 2: System.out.println("Febrero");
+            case 3: System.out.println("Marzo");
+            case 4: System.out.println("Abril");
+            case 5: System.out.println("Mayo");
+            case 6: System.out.println("Junio");
+            case 7: System.out.println("Julio");
+            case 8: System.out.println("Agosto");
+            case 9: System.out.println("Septiembre");
+            case 10: System.out.println("Octubre");
+            case 11: System.out.println("Noviembre");
+            case 12: System.out.println("Diciembre");
+        }
+    }
+}
