f63d1b6b850ca5d2771283a5e958d1db13c25ad5
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-06 11:30:53 +0200

    a

---
+++ Tema 5/Ejercicios Solucionados/eje1/src/main/java/eje1/eje1.java
@@ -0,0 +1,11 @@
+package eje1;
+
+public class eje1 {
+    public static void main(String[] args) {
+        int i = 5;
+        while(i < 15){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ Tema 5/Ejercicios Solucionados/eje2/src/main/java/eje2/ejer2.java
@@ -0,0 +1,11 @@
+package eje2;
+
+public class ejer2 {
+    public static void main(String[] args) {
+        int x = 200;
+        while (x <= 300){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
