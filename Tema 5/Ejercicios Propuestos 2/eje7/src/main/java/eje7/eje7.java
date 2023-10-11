45849ffb502c60b963d44e4900ce05e05bb81f2f
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-11 13:03:00 +0200

    x

--- Tema 5/Ejercicios Propuestos 2/eje7/src/main/java/eje7/eje7.java
+++ Tema 5/Ejercicios Propuestos 2/eje7/src/main/java/eje7/eje7.java
@@ -5,10 +5,25 @@ import java.util.Scanner;
 public class eje7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca el número a convertir:");
-        int x = sc.nextDouble();
-        if (x <= 3999){
-            
+        do{
+            System.out.println("Introduzca un número entre 1 y 3999:");
+            int X = sc.nextDouble();
+        } while ((X<=3999) && (X>=1));
+        int miles = N/1000;
+        int centenas = (N/100)%10;
+        int decimas = (N/10)%10;
+        int unidades = N%10; 
+        String m = "", c = "", d = "", u = "";
+        switch (miles){
+            case 1: m = "M";
+            case 2: m = "MM";
+            case 3: m = "MMM";
+        }
+        switch(centenas){
+            case 1: c = "C";
+            case 2: c = "CC";
+            case 3: c = "CCC";
+
         }
     }
 }
