b3bd7219b3f5a0729914d20e3e5e9c01b08b5f8d
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-16 13:57:08 +0200

    a

--- Tema 5/Ejercicios Adicionales/eje1/src/main/java/eje1/eje1.java
+++ Tema 5/Ejercicios Adicionales/eje1/src/main/java/eje1/eje1.java
@@ -10,14 +10,12 @@ public class eje1 {
         System.out.println("Introduzca el Nº de modulos cursados:");
         int modulos = sc.nextInt();
         double notas = 0;
-        double media;
-        int contador = 0;
-        do{
+        double media = 0;
+        for(int contador = 0; contador<modulos; contador++){
             System.out.println("Introduzca una nota: ");
             notas = sc.nextDouble() + notas;
-            contador++;
             media = notas/modulos;
-        } while (contador<modulos);
+        }
         System.out.println(nombre + ": " + media);
     }
 }
