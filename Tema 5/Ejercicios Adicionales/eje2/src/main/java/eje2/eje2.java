340b99df58bca8354b1238d6684356a363ede8b3
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-16 14:52:01 +0200

    a

--- Tema 5/Ejercicios Adicionales/eje2/src/main/java/eje2/eje2.java
+++ Tema 5/Ejercicios Adicionales/eje2/src/main/java/eje2/eje2.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class eje2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello world!");
+        String confirmacion;
+        int x = 0;
+        do{
+            System.out.println("Introduzca un Nº");
+            x = sc.nextInt() + x;
+            System.out.println("Quiere introducir algún Nº más?");
+            confirmacion = sc.nextLine();
+        } while(!confirmacion.equals("No"));
+        System.out.println("terminado");
     }
 }
