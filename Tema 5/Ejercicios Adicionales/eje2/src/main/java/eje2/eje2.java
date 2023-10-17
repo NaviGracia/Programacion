4c8507524039e959fef9731f47425221f4a428f9
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-17 13:04:15 +0200

    a

--- Tema 5/Ejercicios Adicionales/eje2/src/main/java/eje2/eje2.java
+++ Tema 5/Ejercicios Adicionales/eje2/src/main/java/eje2/eje2.java
@@ -7,12 +7,67 @@ public class eje2 {
         Scanner sc = new Scanner(System.in);
         String confirmacion;
         int x = 0;
+        float media = 0;
+        int cont = 0;
+        int mayor = -2147483647;
+        int menor = 2147483647;
+        int sumapo = 0;
+        int sumane = 0;
+        int canpo = 0;
+        int canne = 0;
+        int sumapar = 0;
+        int sumaimp = 0;
+        int canpar = 0;
+        int canimp = 0;
         do{
             System.out.println("Introduzca un Nº");
-            x = sc.nextInt() + x;
+            x = sc.nextInt();
+            //Calcular Nº Mayor
+            if (x>mayor){
+                mayor = x;
+            }
+            //Calcular Nº Menor
+            if (x<menor){
+                menor = x;
+            }
+            //Contar y Sumar Positivos
+            if (x>0){
+                sumapo = sumapo + x;
+                canpo = canpo + 1;
+            }
+            //Contar y Sumar Negativos
+            if (x<0){
+                sumane = sumane + x;
+                canne = canne + 1;
+            }
+            //Contar y Sumar Pares
+            if (x%2==0){
+                sumapar = sumapar + x;
+                canpar = canpar + 1;
+            }
+            //Contar y Sumar Impares
+            if (x%2==1){
+                sumaimp = sumaimp + x;
+                canimp = canimp + 1;
+            }
+            //Media
+            cont++;
+            media = media + x;
+
             System.out.println("Quiere introducir algún Nº más?");
+            sc.nextLine();
             confirmacion = sc.nextLine();
-        } while(!confirmacion.equals("No"));
+        } while(!confirmacion.equals("no"));
+        media = media/cont;
         System.out.println("terminado");
+        System.out.println(media);
+        System.out.println(mayor);
+        System.out.println(menor);
+        System.out.println(sumapo);
+        System.out.println(sumane);
+        System.out.println(sumapar);
+        System.out.println();
+        System.out.println();
+        System.out.println(canimp);
     }
 }
---
+++ Tema 5/Ejercicios Adicionales/eje3/src/main/java/eje3/eje3.java
@@ -0,0 +1,14 @@
+package eje3;
+
+public class eje3 {
+    public static void main(String[] args) {
+        int x;
+        for(int y = 1; y<=100; y++){
+            if ((y%2=0)&&(y%3=0)){
+                
+            }
+        }
+
+    
+    }
+}
