e78a4ad60572b3864d0b4f8280fb4e9c3d03d4ac
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-19 09:45:03 +0200

    a

--- Tema 5/Ejercicios Adicionales/eje6/src/main/java/eje6/eje6.java
+++ Tema 5/Ejercicios Adicionales/eje6/src/main/java/eje6/eje6.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class eje6 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        System.out.print
+        int contador,I,n;
+        contador = 0;
+        
     }
 }
