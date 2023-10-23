81f89f2f2f8f17389ef2395868c524b5f9e324f5
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-23 14:51:20 +0200

    a

--- Tema 5/Ejercicios Adicionales/eje9/src/main/java/eje9/eje9.java
+++ Tema 5/Ejercicios Adicionales/eje9/src/main/java/eje9/eje9.java
@@ -1,7 +1,16 @@
 package eje9;
 
+import java.util.Scanner;
+
 public class eje9 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un Nº:");
+        int x = sc.nextInt();
+        for(int y = 1; y<=x; y++){
+            for(int j = 1; j<=y; j++){
+            System.out.print(y);
+            }
+        }
     }
 }
