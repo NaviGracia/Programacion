71779cbf8ea8ab87000055c5df841372528a132a
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-25 13:59:48 +0200

    a

--- Tema 5/Ejercicios Adicionales/eje13/src/main/java/eje13/eje13.java
+++ Tema 5/Ejercicios Adicionales/eje13/src/main/java/eje13/eje13.java
@@ -1,10 +1,21 @@
 package eje13;
 
+import java.util.concurrent.TimeUnit;
+
 public class eje13 {
-    public static void main(String[] args) {
-        int horas = 0;
-        int segundos = 0;
-        int minutos = 0;
-        while (horas=!)
+    public static void main(String[] args)throws InterruptedException {
+        int horas;
+        int minutos; 
+        int segundos;
+        String borraAnterior = "\b\b\b\b\b\b\b\b";
+        for(horas = 0; horas<=23; horas++){
+            for(minutos = 0; minutos<=59; minutos++){
+                for(segundos = 0; segundos<=59; segundos++){
+                    System.out.printf("%02d:%02d:%02d",horas,minutos,segundos);
+                    TimeUnit.SECONDS.sleep(1);
+                    System.out.print(borraAnterior);
+                }
+            }
+        }
     }
 }
