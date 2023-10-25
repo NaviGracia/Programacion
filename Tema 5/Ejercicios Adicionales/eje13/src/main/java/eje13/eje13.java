30e168658a7ea969e4b9938228c14bdf3a2b3c21
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-25 12:24:15 +0200

    a

---
+++ Tema 5/Ejercicios Adicionales/eje13/src/main/java/eje13/eje13.java
@@ -0,0 +1,10 @@
+package eje13;
+
+public class eje13 {
+    public static void main(String[] args) {
+        int horas = 0;
+        int segundos = 0;
+        int minutos = 0;
+        while (horas=!)
+    }
+}
