4c01e0c3920f9c40a433e83c3546a55bd59cb116
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-10-27 11:41:52 +0200

    a

--- Tema 11/Ejercicios Propuestos/eje2/src/main/java/eje2/eje2.java
+++ Tema 11/Ejercicios Propuestos/eje2/src/main/java/eje2/eje2.java
@@ -3,19 +3,34 @@ package eje2;
 import java.util.Scanner;
 
 public class eje2 {
-    int fibonacci_iterativo(int posicion){
-        int resultado = 0; 
-        if(posicion>=1){
-            resultado = fibonacci_iterativo((posicion-1)+(posicion-2));
+    int fibonacci_recursivo(int posicion){
+        if((posicion==1)||(posicion==0)){
+            return 1;
         } else
-        resultado = 1;
-        return resultado;
+        return fibonacci_recursivo(posicion-1) + fibonacci_recursivo(posicion-2);
+    }
+
+    int fibonacci_iterativo(int posicion){
+        int preFib2 = 1;
+        int prevFib = 1;
+        int fib = 0;
+        if ((posicion==1)||(posicion==0)) {
+            fib = 1;
+        } else{
+            for (int i=2; i<=posicion; i++){
+                fib = prevFib + preFib2;
+                preFib2 = prevFib;
+                prevFib = fib;
+            }
+        }
+        return fib;
     }
     public static void main(String[] args) {
         eje2 eje2 = new eje2();
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduzca un Nº:");
         int posicion = sc.nextInt();
-        System.out.println("El Nº fibonacci de " + n + " es: " + eje2.fibonacci_iterativo(posicion));
+        System.out.println("El Nº fibonacci recursivo de " + posicion + " es: " + eje2.fibonacci_recursivo(posicion));
+        System.out.println("El Nº fibonacci iterativo de " + posicion + " es: " + eje2.fibonacci_iterativo(posicion));
     }
 }
