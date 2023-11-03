9052162887efad12352015af8d7b01e1b5da65f8
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-11-03 10:12:06 +0100

    a

--- Tema 11/Ejercicios Propuestos/eje5/src/main/java/eje5/eje5.java
+++ Tema 11/Ejercicios Propuestos/eje5/src/main/java/eje5/eje5.java
@@ -7,15 +7,17 @@ public class eje5 {
         if ((columnas==1)) {
             return 1;
         } if ((columnas>1)&&(contador==columnas)) {
-            contador = contador+2;
             return 1;
         }if (linea==2) {
             return 1;
+        }if ((columnas>contador-2)&&(contador!=columnas)) {
+            int resultado = (funcion(linea-1, columnas-2, contador-2) + funcion(linea-1, columnas-1, contador-2));
+            return resultado;
         }if (columnas==2) {
-            int resultado = (funcion((linea-1), columnas, contador) + funcion(linea-1, columnas-1, contador));
+            int resultado = (funcion((linea-1), columnas, contador-2) + funcion(linea-1, columnas-1, contador-2));
             return resultado;
         }else{
-            int resultado = (funcion((linea - 1), (columnas), contador) + funcion((linea - 1), (columnas-1), contador) + funcion((linea - 1), (columnas-2), contador));
+            int resultado = (funcion((linea - 1), (columnas), contador-2) + funcion((linea - 1), (columnas-1), contador-2) + funcion((linea - 1), (columnas-2), contador-2));
             return resultado;
         }
         
@@ -29,14 +31,14 @@ public class eje5 {
     1
     111
     12331
-    1368771
+    1368779
     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         eje5 eje5 = new eje5();
         int linea;
         int columna;
-        int contador =  3;
+        int contador =  1;
         System.out.println("Introduzca el Nº:");
         int valorTotal = sc.nextInt();
         for (linea = 1; linea<valorTotal; linea++) {
@@ -46,6 +48,7 @@ public class eje5 {
             for(columna=1; columna<2*linea; columna++){
                 System.out.print(eje5.funcion(linea, columna,contador));
             }
+            contador = contador+2;
             System.out.println("");
         }
     }
