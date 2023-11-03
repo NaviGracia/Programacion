854da4f7d77d7974294ee53cc0a271b726e67391
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-11-03 08:22:55 +0100

    a

--- Tema 11/Ejercicios Propuestos/eje5/src/main/java/eje5/eje5.java
+++ Tema 11/Ejercicios Propuestos/eje5/src/main/java/eje5/eje5.java
@@ -3,20 +3,22 @@ package eje5;
 import java.util.Scanner;
 
 public class eje5 {
-    public static int funcion (int linea, int columnas){
+    public static int funcion (int linea, int columnas, int contador){
         if ((columnas==1)) {
             return 1;
-        } if ((columnas>1)&&(columnas%2==1)) {
+        } if ((columnas>1)&&(contador==columnas)) {
+            contador = contador+2;
             return 1;
         }if (linea==2) {
             return 1;
         }if (columnas==2) {
-            int resultado = (funcion((linea-1), columnas) + funcion(linea-1, columnas-1));
+            int resultado = (funcion((linea-1), columnas, contador) + funcion(linea-1, columnas-1, contador));
             return resultado;
         }else{
-            int resultado = (funcion((linea - 1), (columnas)) + funcion((linea - 1), (columnas-1)) + funcion((linea - 1), (columnas-2)));
+            int resultado = (funcion((linea - 1), (columnas), contador) + funcion((linea - 1), (columnas-1), contador) + funcion((linea - 1), (columnas-2), contador));
             return resultado;
         }
+        
     }
     /*
     1
@@ -34,6 +36,7 @@ public class eje5 {
         eje5 eje5 = new eje5();
         int linea;
         int columna;
+        int contador =  3;
         System.out.println("Introduzca el Nº:");
         int valorTotal = sc.nextInt();
         for (linea = 1; linea<valorTotal; linea++) {
@@ -41,7 +44,7 @@ public class eje5 {
                 System.out.print(" ");
             }
             for(columna=1; columna<2*linea; columna++){
-                System.out.print(eje5.funcion(linea, columna));
+                System.out.print(eje5.funcion(linea, columna,contador));
             }
             System.out.println("");
         }
