5b96d5208a8bf7c635a686993af0e48f493e39e9
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-11-08 13:13:11 +0100

    a

--- Tema 11/Ejercicios Libro/eje5/src/main/java/eje5/eje5.java
+++ Tema 11/Ejercicios Libro/eje5/src/main/java/eje5/eje5.java
@@ -3,11 +3,33 @@ package eje5;
 import java.util.Scanner;
 
 public class eje5 {
+    int division (int dividendo, int divisor, int cociente){
+        if (divisor>dividendo) {
+            return cociente;
+        }else{
+            cociente = division(dividendo-divisor, divisor,cociente+1);
+            return cociente;
+        }
+    }
+    int resto (int dividendo, int divisor){
+        int resto = 0;
+        if (divisor>dividendo) {
+            resto = dividendo;
+            return resto;
+        } else{
+            resto = resto(dividendo-divisor, divisor);
+            return resto;
+        }
+        
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         eje5 eje5 = new eje5();
-        System.out.println("Intr");
-        int n;
-        int m;
+        System.out.println("Introduzca el dividendo:");
+        int dividendo = sc.nextInt();
+        System.out.println("Introduzca el divisor:");
+        int divisor = sc.nextInt();
+        int cociente = 0;
+        System.out.println(dividendo + " dividido " + divisor + " es: " + eje5.division(dividendo, divisor, cociente) + " y resto de " + eje5.resto(dividendo, divisor));
     }
 }
