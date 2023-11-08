bee8ce551bd8a9789ea31151d064d11132da6f70
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-11-08 13:55:40 +0100

    a

--- Tema 11/Ejercicios Libro/eje6/src/main/java/eje6/eje6.java
+++ Tema 11/Ejercicios Libro/eje6/src/main/java/eje6/eje6.java
@@ -1,11 +1,26 @@
 package eje6;
 
 public class eje6 {
+    long multi (long [] vector1, int pos){
+        if (pos<vector1.length) {
+            return vector1[pos] * multi(vector1, pos+1);
+            //vector2(vector2.length-1);
+            
+        }
+        return 1;
+/* 
+        multiplicacion = multiplicacion * multi(vector1.length-1);
+        return multiplicacion;*/
+    }
     public static void main(String[] args) {
-        int [] vector1;
-        vector1 = new vector1[15];
-        int [] vector2;
-        vector2 = new vector2 [vector1.length-1];
-        v2(vector2.length-1);
+        eje6 eje6 = new eje6();
+        long [] vector1;
+        vector1 = new long[15];
+        for (int x = 0; x<vector1.length;x++){
+            vector1[x] = (int)(Math.random()*9)+1;
+        }
+        
+        System.out.println("La multiplicación de los Nº generados aleatoriamente es: " + eje6.multi(vector1, 0));
+        
     }
 }
