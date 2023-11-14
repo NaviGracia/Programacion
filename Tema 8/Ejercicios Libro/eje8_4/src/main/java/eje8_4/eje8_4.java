8be9146485a74c85762ced401ec5a3bd83c99d93
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-11-14 08:49:02 +0100

    a

--- Tema 8/Ejercicios Libro/eje8_4/src/main/java/eje8_4/eje8_4.java
+++ Tema 8/Ejercicios Libro/eje8_4/src/main/java/eje8_4/eje8_4.java
@@ -3,18 +3,22 @@ package eje8_4;
 public class eje8_4 {
 
     public static void main(String[] args) {
-        int cont = 0;
         int prueba = 0;
-        for(int posicion = 0; posicion<args.length; cont++){
-            for(int posicionComparacion = 0; posicionComparacion<args.length; posicionComparacion++){
+        int nPalabrasDistintas = 1;
+        for(int posicion = 0; posicion<args.length; posicion++){
+            int cont = 0;
+            for(int posicionComparacion = 0; posicionComparacion<posicion; posicionComparacion++){
+                
                 if (args[posicion].compareToIgnoreCase(args[posicionComparacion])==0) {
-                    cont = cont+1;
-                }else{
                     prueba=prueba++;
-                }/*Problemas con la repetición: Bucle Infinito*/ */
-                System.out.println(args[posicion].compareToIgnoreCase(args[posicionComparacion]));
+                }else{
+                    cont++;
+                }
+                if (cont==posicion) {
+                    nPalabrasDistintas++;
+                }
             }
         }
-        System.out.println(cont);
+        System.out.println("El Nº de palabras diferentes es: " + nPalabrasDistintas);
     }
 }
