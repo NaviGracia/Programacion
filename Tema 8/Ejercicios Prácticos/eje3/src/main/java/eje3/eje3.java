4c9af93994f859f1026410652503249aebcbc467
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-11-14 11:36:24 +0100

    a

---
+++ Tema 8/Ejercicios Prácticos/eje3/src/main/java/eje3/eje3.java
@@ -0,0 +1,15 @@
+package eje3;
+
+public class eje3 {
+    public static void main(String[] args) {
+        int[][] matriz;
+        matriz = new int[5][8];
+        for(int lineas = 1; lineas<5; lineas++){
+            for(int columnas = 1; columnas<8; columnas++){
+                matriz[lineas][columnas] = (int)(Math.random()*99)+1;
+                System.out.print(matriz[lineas][columnas] + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
