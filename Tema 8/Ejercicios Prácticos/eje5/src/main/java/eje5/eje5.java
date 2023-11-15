e6e1661026ca3d366006cee08175c045a0547b54
NaviGracia <ivagartor2@alu.edu.gva.es>
2023-11-15 14:44:21 +0100

    a

--- Tema 8/Ejercicios Prácticos/eje5/src/main/java/eje5/eje5.java
+++ Tema 8/Ejercicios Prácticos/eje5/src/main/java/eje5/eje5.java
@@ -1,11 +1,31 @@
 package eje5;
 
+import java.util.Scanner;
+
 public class eje5 {
-    public static 
+    static int[] v;
+    static int lon;
+
+   /*  public asignarDatos(int[] v){
+        for(int posicion = 0; posicion<v.length; posicion++){
+
+    }*/
+
+    int[] solicitarTamanyo() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca la longitud que desea del array:");
+        lon = sc.nextInt();
+        v = new int[lon];
+        return v;
+    }
 
     public static void main(String[] args) {
-        int lon;
-        int[] v;
+        Scanner sc = new Scanner(System.in);
+        int tamanyo=solicitarTamanyo(v, lon);
         
+        System.out.println(v.length);
     }
+
+    
+
 }
