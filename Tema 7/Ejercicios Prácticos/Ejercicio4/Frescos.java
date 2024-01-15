54d02bc76a710806dbe6647e80a9a8b06449debd
Navi <10724685@ieselgrao.org>
2024-01-15 17:43:45 +0100

    faltan subclases congelados

---
+++ Tema 7/Ejercicios Prácticos/Ejercicio4/Frescos.java
@@ -0,0 +1,8 @@
+package Ejercicio4;
+
+public class Frescos extends Productos {
+
+    public Frescos(int numLote, String fechaEnvasado, String fechaCaducidad, String paisOrigen) {
+        super(numLote, fechaEnvasado, fechaCaducidad, paisOrigen);
+    }
+}
---
+++ Tema 7/Ejercicios Prácticos/Ejercicio4/Refrigerados.java
@@ -0,0 +1,32 @@
+package Ejercicio4;
+
+    public class Refrigerados extends Productos{
+        int codigoOrganismoSupervision;
+        int temperaturaRecomendada;
+
+        public Refrigerados(int numLote, String fechaEnvasado, String fechaCaducidad, String paisOrigen,
+                int codigoOrganismoSupervision, int temperaturaRecomendada) {
+            super(numLote, fechaEnvasado, fechaCaducidad, paisOrigen);
+            this.codigoOrganismoSupervision = codigoOrganismoSupervision;
+            this.temperaturaRecomendada = temperaturaRecomendada;
+        }
+        
+        public int getCodigoOrganismoSupervision() {
+            return codigoOrganismoSupervision;
+        }
+        public void setCodigoOrganismoSupervision(int codigoOrganismoSupervision) {
+            this.codigoOrganismoSupervision = codigoOrganismoSupervision;
+        }
+        public int getTemperaturaRecomendada() {
+            return temperaturaRecomendada;
+        }
+        public void setTemperaturaRecomendada(int temperaturaRecomendada) {
+            this.temperaturaRecomendada = temperaturaRecomendada;
+        }
+
+        @Override
+        public String toString() {
+            // TODO Auto-generated method stub
+            return super.toString() + "\n Temperatura Recomendada: " + temperaturaRecomendada + " ºC" + "\n Código Organismo Supervisión Alimenticia: " + codigoOrganismoSupervision;
+        }
+    }
