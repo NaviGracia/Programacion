035b24b5d9ebefa3c9ce43e753855b1bf0e69a0c
Navi <10724685@ieselgrao.org>
2024-01-22 15:43:00 +0100

    7 acabado

---
+++ Tema 7/Ejercicios Prácticos/Ejercicio7/Pez.java
@@ -0,0 +1,35 @@
+package Ejercicio7;
+
+public class Pez {
+    String nombre;
+    private int numPeces = 1;
+
+    public Pez(String nombre) {
+        this.nombre = nombre;
+        numPeces+=1;
+    }
+
+    public Pez(Pez p1){
+        this.nombre = p1.nombre;
+        this.numPeces = p1.numPeces;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumPeces() {
+        return numPeces;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        Pez pezComp = (Pez)obj;
+        return this.getNombre().equals(pezComp.getNombre());
+    }
+}
