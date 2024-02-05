754dab99b69b5c9111cce8f8d2588b22b9b61c47
Navi <10724685@ieselgrao.org>
2024-02-05 18:10:29 +0100

    a

--- Tema 7/Ejercicios Prácticos/Ejercicio9/Prueba.java
+++ Tema 7/Ejercicios Prácticos/Ejercicio9/Prueba.java
@@ -3,13 +3,7 @@ package Ejercicio9;
 public class Prueba {
     protected String nombre;
     protected int ID;
-<<<<<<< HEAD
     public String getIdent ( ) { return nombre;}
     public int getIdent ( ) { return ID;}
     //No porque no puede haber varios métodos con el mismo nombre en una misma clase
-=======
-    public String getIdent(){return nombre;};
-    public int getIdent(){return ID;};
-    /*El código no compilará por la repetición del método getIdent. */
->>>>>>> bb775caf22dc82b4977673fad22080f5fb5ae4a5
 }
