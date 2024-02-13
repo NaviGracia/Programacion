a71bb850e8042c6729ff8952ca4e5725df652a94
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-02-13 11:54:45 +0100

    a

--- Tema 9/Ejercicios ArrayList/Ejercicio1/Ejercicio1.java
+++ Tema 9/Ejercicios ArrayList/Ejercicio1/Ejercicio1.java
@@ -1,10 +1,42 @@
 package Ejercicio1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Iterator;
 
 public class Ejercicio1 {
+    static Scanner sc = new Scanner(System.in);
+    public static ArrayList leerValores(ArrayList arrayEnteros){
+        int num = 0;
+        do{
+            System.out.println("Introduzca el Nº" + arrayEnteros.size() + ":");
+            num = sc.nextInt();
+            if (num != -99) {
+                arrayEnteros.add(num);   
+            }
+        }while(num != -99);
+        return arrayEnteros;
+    }
+
+    public static int calcularSuma(ArrayList arrayEnteros){
+        Iterator<Integer> iterador = arrayEnteros.iterator();
+        int suma = 0;
+        while (iterador.hasNext()) {
+            suma+= iterador.next();
+        }
+        return suma;
+    }
+
+    public static void mostrarResultados(ArrayList arrayEnteros, int suma, int media){
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         ArrayList<Integer> arrayEnteros = new ArrayList<>();
-        do
+        leerValores(arrayEnteros);
+        int suma = calcularSuma(arrayEnteros);
+        int media = calcularSuma(arrayEnteros)/arrayEnteros.size();
+        mostrarResultados(arrayEnteros, suma, media);
+        
     }
 }
