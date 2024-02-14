68d117b200a6b1ea54ace2c69e146d247c0f0d85
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-02-14 13:22:36 +0100

    as

--- Tema 9/Ejercicios ArrayList/Ejercicio1/Ejercicio1.java
+++ Tema 9/Ejercicios ArrayList/Ejercicio1/Ejercicio1.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 
 public class Ejercicio1 {
     static Scanner sc = new Scanner(System.in);
-    public static ArrayList leerValores(ArrayList arrayEnteros){
+    public static ArrayList<Integer> leerValores(ArrayList<Integer> arrayEnteros){
         int num = 0;
         do{
             System.out.println("Introduzca el Nº" + arrayEnteros.size() + ":");
@@ -18,7 +18,7 @@ public class Ejercicio1 {
         return arrayEnteros;
     }
 
-    public static int calcularSuma(ArrayList arrayEnteros){
+    public static int calcularSuma(ArrayList<Integer> arrayEnteros){
         Iterator<Integer> iterador = arrayEnteros.iterator();
         int suma = 0;
         while (iterador.hasNext()) {
@@ -27,8 +27,19 @@ public class Ejercicio1 {
         return suma;
     }
 
-    public static void mostrarResultados(ArrayList arrayEnteros, int suma, int media){
-        System.out.println();
+    public static void mostrarResultados(ArrayList<Integer> arrayEnteros, int suma, int media){
+        for(int cont = 0; cont <arrayEnteros.size(); cont++){
+            System.out.println(arrayEnteros.get(cont));
+        }
+        System.out.println("Suma: " + suma);
+        System.out.println("Media:" + media);
+        System.out.println("Nº > a la media: ");
+        for(Integer cont = 0; cont < arrayEnteros.size(); cont++){
+            Integer numeroComp = arrayEnteros.get(cont);
+            if (numeroComp>media) {
+                System.out.println(numeroComp);
+            }
+        }
     }
 
     public static void main(String[] args) {
