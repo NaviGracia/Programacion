c13ee3430fafa2717484f21f6d0fa484091ee5ed
NaviGracia <ivagartor2@alu.edu.gva.es>
2024-02-19 14:53:43 +0100

    c

---
+++ Tema 9/Ejercicios ArrayList/Ejercicio4/App.java
@@ -0,0 +1,9 @@
+package Ejercicio4;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        ArrayList<>
+    }
+}
---
+++ Tema 9/Ejercicios ArrayList/Ejercicio4/Persona.java
@@ -0,0 +1,10 @@
+package Ejercicio4;
+
+public class Persona {
+    private String dni;
+    private String apellidos;
+    private String nombre;
+    private int edad;
+    private double peso;
+    private char sexo,
+}
